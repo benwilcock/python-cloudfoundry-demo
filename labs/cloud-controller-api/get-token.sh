5ecd9fbc6df9430ce5b83fa32f75b7db2373b0e3
benwilcock <benwilcock@gmail.com>
2016-08-18 17:06:12 +0100

    removed the labs folder. added latest pip to reqs. added jenkins to docker-compose. added hello-html staticfile app.

--- labs/cloud-controller-api/get-token.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Gets an API token for a local PCF DEV instance and puts it in a file called 'token.json'.
-curl -k -H 'AUTHORIZATION: Basic Y2Y6' -d 'username=user&password=pass&grant_type=password' https://uaa.local.pcfdev.io/oauth/token > token.json
