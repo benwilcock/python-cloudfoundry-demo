a69327a7f0a87a2e543f044645138dd8d7a8100c
benwilcock <benwilcock@gmail.com>
2016-09-15 19:27:40 +0100

    Moved original connector to the help folder. Made the default connector require further config.

--- help/connector.py
+++ help/connector.py
@@ -8,10 +8,11 @@ conn = None
 def getDatabaseUri():
 
     # Extract VCAP_SERVICES
-    vcap_services = None
+    vcap_services = os.environ.get('VCAP_SERVICES',
+                                   '{"user-provided": [{"credentials": {"uri": "postgres://postgres:password@192.168.11.1:5432/postgres"}, "tags": [], "label": "user-provided", "name": "postgresql"}]}')
 
     if vcap_services is None:
-        print('The environment variables have not been imported.')
+        print('The VCAP_SERVICES environment variable does not appear to be set.')
         return None
 
     decoded_config = json.loads(vcap_services)
@@ -21,7 +22,7 @@ def getDatabaseUri():
         if decoded_config[key][0]['name'] == 'postgresql':
             creds = decoded_config[key][0]['credentials']
             uri = creds['uri']
-            print('Identified postgres DATABASE uri: ' + uri)
+            print('Identified postgres uri: ' + uri)
             return uri
 
 
@@ -41,7 +42,7 @@ def getDatabaseConnection():
             print('PyCarsAPI Database Connection Attempt Failed: ')
             return None
     else:
-        print('DATABASE uri could not be found. Has the service been bound correctly?')
+        print('Database Uri was unexpectedly empty: ' + str(uri))
         return None
 
 if __name__ == '__main__':
